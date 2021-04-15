3c32dd8a723ab75d1ec0b6ad8225be6502a0fd52
MaxwellSam <81492409+MaxwellSam@users.noreply.github.com>
2021-04-15 03:42:12 +0200

    rectif et optimisation de la place

--- Extract/plotSignal.r
+++ Extract/plotSignal.r
@@ -3,8 +3,8 @@
 # authors : Belmahi Asmae, Maxwell Sam
 # date : 14/04/2021	
 dev.new(width=7, height=4)
-df=read.table("channel_2_large.txt",header=T)
-plot(df[,1], df[,2], main = "Evolution of action potential on 60 seconds",
+df=read.table("channel_2.txt",header=T)
+plot(df[,1], df[,2], main = "Evolution of action potential on the first 8 seconds",
 	type="l", 
 	col="#3182bd", 
 	xlab = "time [ms]", 
--- M_average/plotMvAvg.r
+++ M_average/plotMvAvg.r
@@ -3,9 +3,9 @@
 # authors : Belmahi Asmae, Maxwell Sam
 # date : 14/04/2021	
 dev.new("AVGplot.pdf",width=7, height=4)
-df1=read.table("channel_2_1.txt",header=T)
-df2=read.table("mvAvg_channel_2_1.txt", header=T)
-plot(df1[,1],df1[,2], main = "Evolution of action potential during the first 7 seconds\nand baseline",
+df1=read.table("channel_2.txt",header=T)
+df2=read.table("mvAvg_channel_2.txt", header=T)
+plot(df1[,1],df1[,2], main = "Evolution of action potential on the first 8 seconds\nand its baseline",
 	type="l", 
 	col="#3182bd", 
 	xlab = "time [s.10E-3]", 
